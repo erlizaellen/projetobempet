bae68df6a190b83378fa2e6275b5feddc2e62ec7
erlizaellen <erlizaellen@gmail.com>
2025-05-14 11:53:47 -0300

    ajeitando as div clinicas

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,35 @@
 
 
 import PageWrapper from "@/components/PageWrapper";
+import Image from "next/image";
 
 export default function Home(){
     return (
         <PageWrapper>
-            <div className="w-[123px] h-[123px]"></div>
+            <div className="w-full h-auto flex flex-col  mt-4 ">
+                <div 
+                  className="w-full h-[350px] flex flex-col items-center justify-center bg-[#f3f598]">
+                  <h1 className="text-[50px] font-semibold text-green-900 ">Seja Bem Vindo</h1>
+                  <p className="text-[30px] font-semibold text-green-900">Cuidando do seu melhor amigo com carinho que ele merece ! </p>
+                </div>
+
+                <div className="w-full h-auto flex gap-4 p-4 items-center justify-center ">
+                    <div className="w-[300px] h-[200px] border border-2 "></div>
+                    <div className="w-[300px] h-[200px] border border-2 "></div>
+                    <div className="w-[300px] h-[200px] border border-2 "></div>
+                    <div className="w-[300px] h-[200px] border border-2 "></div>
+
+                </div>
+                
+                <div className="w-full h-auto flex gap-4 p-4 items-center justify-center ">
+                    <div className="w-[300px] h-[200px] border border-2 "></div>
+                    <div className="w-[300px] h-[200px] border border-2 "></div>
+                    <div className="w-[300px] h-[200px] border border-2 "></div>
+                    <div className="w-[300px] h-[200px] border border-2 "></div>
+
+                </div>
+                
+            </div>
         </PageWrapper>
     )
 }
