28448e4ae11520a5b1ea2e9fea20dbbb2ad2ee4a
hannafreitasc <hannafreitascc@gmail.com>
2025-05-14 11:56:02 -0300

    criando a pagina login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,7 +3,18 @@ import PageWrapper from "@/components/PageWrapper";
 export default function Login() {
     return(
         <PageWrapper>
-        <div className="">teste login</div>
+        <div className="w-full h-full flex">
+            <div className="w-[600px] h-[90vh] bg-[#5bf356c2] flex mt-8 ml-10 rounded-xl">
+                <div className="w-full h-[100px] flex items-center justify-center">
+                   <h1 className=" font-bold text-[40px] text-[#215f1f]">LOGIN</h1>
+                </div>
+            </div>
+            <div className="w-full h-full flex">
+                <img src="" alt="" />
+            </div>
+                 
+        </div>
+        
         </PageWrapper>
     )
 }
