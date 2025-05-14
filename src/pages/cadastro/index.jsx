a076f9c1a8214a21692d71697003d781fb3dba06
Lid1981 <lidramos1981@gmail.com>
2025-05-14 11:45:55 -0300

    Separação Dev Cadastro

--- src/pages/cadastro/index.jsx
+++ src/pages/cadastro/index.jsx
@@ -5,7 +5,16 @@ export default function Cadastro() {
     
     return(
         <PageWrapper>
-        <div className="w-full h-full py-[40px] px-[70px] flex flex-col bg-amber-500">CADASTRO</div>
+        <div className="w-full h-full flex gap-40 p-40 bg-[#5bf356c2]">
+            <div className="w-[50%] h-[70vh] bg-[#5bf356c2] rounded-xl">
+                <div className="w-full h-[10vh] bg-amber-500 rounded-2xl">
+                    <h1 className="font-bold text-white text-[30px] justify-center flex">Cadastre-se</h1>
+                </div>
+                <div className="w-full h-[80vh] rounded-xl bg-amber-950"></div>
+                <div className="w-full h-[10vh] bg-amber-700"></div>
+            </div>
+            <div className="w-[50%] h-[70vh] rounded-xl bg-white"></div>
+        </div>
         </PageWrapper>
     )
 }
