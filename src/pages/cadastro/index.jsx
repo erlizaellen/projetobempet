889f57b4b4b3b68849db38d770910820f683ad03
Lid1981 <lidramos1981@gmail.com>
2025-05-14 10:46:57 -0300

    Cadastro

--- src/pages/cadastro/index.jsx
+++ src/pages/cadastro/index.jsx
@@ -1,9 +1,11 @@
 import PageWrapper from "@/components/PageWrapper";
 
 export default function Cadastro() {
+    
+    
     return(
         <PageWrapper>
-        <div className="">CADASTRO</div>
+        <div className="w-full h-full py-[40px] px-[70px] flex flex-col bg-amber-500">CADASTRO</div>
         </PageWrapper>
     )
 }
