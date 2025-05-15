00c285ec98340d7798cba407839b52cba6bd2850
joa242312 <joaogabrielss2006@gmail.com>
2025-05-15 11:56:16 -0300

    add

--- src/components/PageWrapper/index.jsx
+++ src/components/PageWrapper/index.jsx
@@ -1,4 +1,5 @@
 import Footer from "../Footer"
+
 import Header from "../Header"
 
 export default function PageWrapper({children}){
--- src/pages/vantagens/index.jsx
+++ src/pages/vantagens/index.jsx
@@ -3,7 +3,9 @@ import PageWrapper from "@/components/PageWrapper";
 export default function vantagens() {
     return(
         <PageWrapper>
-        <div className="">VANTAGENS</div>
+        <div className="w-full h-[70px] bg-amber-300">
+            <h1>VANTAGENS</h1>
+        </div>
         </PageWrapper>
     )
 }
