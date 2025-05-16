c3465dde9a810895f5f36b1a6744bcc78592b57f
hannafreitasc <hannafreitascc@gmail.com>
2025-05-16 11:53:12 -0300

    ajustando titulo card vantagens

--- src/components/CardVantagens/index.jsx
+++ src/components/CardVantagens/index.jsx
@@ -10,7 +10,7 @@ export default function CardVantagens({ texto, titulo, className }) {
                 onMouseOut={() => setHover(false)}
                 className={`relative hover:shadow-md overflow-hidden w-[80%] h-[150px] flex flex-col ml-20 bg-[#ffc0cb] rounded-lg mt-10 ${className}`}>
                 <div className={`${hover ? "w-[90px] h-[90px]" : "w-[80px] h-[80px]"} rounded-full transition-all ease-in-out duration-300 bg-[#0000003b] absolute top-[-30px] right-[-40px]`}></div>
-                <h1  className="font-bold text-[20px] ml-4 flex justify-center  mt-4 bg-amber-100 w-[40%] rounded-full">{titulo}</h1>
+                <h1  className="font-bold text-[20px] ml-4 flex justify-center max-w-[60%]  mt-4 bg-amber-100 px-4 rounded-full">{titulo}</h1>
                 <p className="font-sm text-[20px] ml-6 mt-3">
                     {texto}
                 </p>
