975138f471c753540aa9b73aadb652d41118efaa
ramirospinheiro <ramiropinheiro@gmail.com>
2025-05-19 11:34:18 -0300

    alternar de cartao para pix com ternario

--- src/pages/assinatura/index.jsx
+++ src/pages/assinatura/index.jsx
@@ -3,10 +3,14 @@ import { FaPix } from "react-icons/fa6";
 import { FaRegCreditCard } from "react-icons/fa";
 import { TbPawFilled } from "react-icons/tb";
 import { LuPawPrint } from "react-icons/lu";
+import { useState } from "react";
 
 
 
 export default function assinatura() {
+    const [opcao, setOpcao] = useState("Cartao")
+    
+
     return(
         <PageWrapper>
         <div className="w-full h-auto flex justify-center">
@@ -17,67 +21,82 @@ export default function assinatura() {
                 <TbPawFilled size={20}/>
                 </div>
                 <div className="w-full h-[20%]] flex gap-3 mb-4">
-                    <button className="w-[50%] h-[40px] flex justify-center items-center gap-3 rounded-md border border-[#215f1f] bg-[#5bf356c2] cursor-pointer hover:bg-[#5bf356c2]/80">
+                    <button 
+                        onClick={() => setOpcao('Cartao')}
+                        className="w-[50%] h-[40px] flex justify-center items-center gap-3 rounded-md border border-[#215f1f] bg-[#5bf356c2] cursor-pointer hover:bg-[#5bf356c2]/80">
                         <FaRegCreditCard size={20} className="text-[#215f1f]"/>
                         <p className="text-[20px] font-bold text-[#215f1f]">Cartão</p>
                         </button>
-                    <button className="w-[50%] h-[40px] flex justify-center items-center gap-3 rounded-md border border-[#215f1f] bg-[#5bf356c2] cursor-pointer hover:bg-[#5bf356c2]/80">
+                    <button 
+                        onClick={() => setOpcao('Pix')}
+                        className="w-[50%] h-[40px] flex justify-center items-center gap-3 rounded-md border border-[#215f1f] bg-[#5bf356c2] cursor-pointer hover:bg-[#5bf356c2]/80">
                         <FaPix size={20} className="text-[#215f1f]"/>
                         <p className="text-[20px] font-bold text-[#215f1f]">Pix</p>  
                     </button>
                 </div>
-                <div className="w-full h-[80%]">
-                    <div className="w-full flex flex-col">
-                        <label className="text-[20px] font-bold text-[#215f1f]">
-                            Nome no cartão
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Fulano de Tal"
-                            className="w-full h-[40px] rounded-md bg-[#eeeeee] hover:bg-[#ADEEAB] pl-2"
-                        />
-                    </div>
-                    <div>
-                    <label className="text-[20px] font-bold text-[#215f1f]">
-                            Número do cartão
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="0123 4567 8910 1112"
-                            className="w-full h-[40px] rounded-md bg-[#eeeeee] hover:bg-[#ADEEAB] pl-2"
-                        />
-                    </div>
-                    <div className="w-full flex flex-row">
-                        <div className="w-[70%] pr-2">
-                        <label className="text-[20px] font-bold text-[#215f1f]">
-                            Expiração
-                        </label>
-                        <div className="flex flex-row gap-2">
-                            <input 
-                                type="number" 
-                                placeholder="00"
-                                className="w-full h-[40px] rounded-md bg-[#eeeeee] hover:bg-[#ADEEAB] pl-2"
-                            />
+                {
+                    opcao == "Cartao" ? (
+                        <div className="w-full h-[80%]">
+                        <div className="w-full flex flex-col">
+                            <label className="text-[20px] font-bold text-[#215f1f]">
+                                Nome no cartão
+                            </label>
                             <input
-                                type="number"
-                                placeholder="00"
+                                type="text"
+                                placeholder="Fulano de Tal"
                                 className="w-full h-[40px] rounded-md bg-[#eeeeee] hover:bg-[#ADEEAB] pl-2"
                             />
                         </div>
-                        </div>
-                        <div className="w-[30%]">
+                        <div>
                         <label className="text-[20px] font-bold text-[#215f1f]">
-                            CVV
-                        </label>
-                        <input
-                                type="number"
-                                placeholder="000"
+                                Número do cartão
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="0123 4567 8910 1112"
                                 className="w-full h-[40px] rounded-md bg-[#eeeeee] hover:bg-[#ADEEAB] pl-2"
                             />
+                        </div>
+                        <div className="w-full flex flex-row">
+                            <div className="w-[70%] pr-2">
+                            <label className="text-[20px] font-bold text-[#215f1f]">
+                                Expiração
+                            </label>
+                            <div className="flex flex-row gap-2">
+                                <input 
+                                    type="number" 
+                                    placeholder="00"
+                                    className="w-full h-[40px] rounded-md bg-[#eeeeee] hover:bg-[#ADEEAB] pl-2"
+                                />
+                                <input
+                                    type="number"
+                                    placeholder="00"
+                                    className="w-full h-[40px] rounded-md bg-[#eeeeee] hover:bg-[#ADEEAB] pl-2"
+                                />
+                            </div>
+                            </div>
+                            <div className="w-[30%]">
+                            <label className="text-[20px] font-bold text-[#215f1f]">
+                                CVV
+                            </label>
+                            <input
+                                    type="number"
+                                    placeholder="000"
+                                    className="w-full h-[40px] rounded-md bg-[#eeeeee] hover:bg-[#ADEEAB] pl-2"
+                                />
+                            </div>
                         </div>
                     </div>
-                </div>
-                
+      
+                    ) :
+                    (     
+                        <div className="w-full h-[80%] break-words">
+                            <img src="/img/QRCode.jpeg"alt="" />
+                            <p className="max-w-[300px]">00020126430014br.gov.bcb.pix0114+55859964602000203Pix5204000053039865802BR5921RAMIRO SILVA PINHEIRO6009FORTALEZA622905258qbcECvUnn2ibV5j47CbzoBEx63042143</p>
+                        </div>
+                    )
+                    
+                }              
                 <button className="w-[100%] h-[50px] flex justify-center items-center gap-3 rounded-md border border-[#215f1f] bg-[#5bf356c2] text-[20px] font-bold text-[#215f1f] cursor-pointer hover:bg-[#5bf356c2]/80">
                     Efetuar pagamento
                     <LuPawPrint />
