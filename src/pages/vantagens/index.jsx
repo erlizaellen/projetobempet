6796ec4315d305f954884cfda08a9236e13429a8
joa242312 <joaogabrielss2006@gmail.com>
2025-05-20 11:28:19 -0300

    responsive

--- src/pages/vantagens/index.jsx
+++ src/pages/vantagens/index.jsx
@@ -5,7 +5,7 @@ import { TbPawFilled } from "react-icons/tb";
 export default function vantagens() {
     return (
         <PageWrapper>
-            <div className="w-full p-8 text-center rp-amber-500  max-h-dvh">
+            <div className="w-full p-8 text-center rp-amber-500 sm:p-8 lg:p-16  ">
                 <div className="flex items-center justify-center">
                     <h1 className="text-[50px] font-bold text-[#215f1f]/70"> Vantagens</h1>
                 </div>
@@ -14,20 +14,20 @@ export default function vantagens() {
                     para você e seu pet. <br/> Uma experiência para garantir o maior cuidado.</p>
             </div>
             <div className="w-full h-auto flex flex-wrap ">
-                <div className="w-full h-full flex flex-wrap rb:bg-red-600">
+                <div className="w-full h-full flex flex-wrap max-md:grig">
 
                     <CardVantagens titulo={"Tudo para seu Pet"} texto={"Rações e acessórios que seus pets vão adorar. Petshops afiliados na sua região."} />
                     <CardVantagens className={"bg-blue-200 bg"} titulo={"Parcerias em expansão"} texto={"A cada semana, novas clínicas veterinárias e petshop são adicionado a nossa rede de parceiros com ofertas exclusivas."} />
                     <CardVantagens className={"bg-amber-200"} titulo={"Localização inteligente"} texto={"Encontre clínicas de atendimento próximas da sua casa, facilitando o acessos aos melhores serviços para seu pet."} />
                     <CardVantagens className={"bg-lime-200"} titulo={"Benefício por assinatura"} texto={"Assinando o Bem Pet,você automaticamente tem acesso a deversos benefícios exclusivos"} />
                 </div>
-                <div className="w-full h-[50px] flex items-center justify-center  mt-8">
+                <div className="w-full h-full flex items-center justify-center ">
 
                     <button
                         type="submit"
-                        className="flex-row  flex items-center justify-center w-[15%] h-[50px] 
+                        className="flex items-center justify-center max-md:w-full w-[15%]  h-[50px] 
                          mp:bg text-white font-bold bg-[#215f1f] mt-12 text-[20px] rounded-full
-                            cursor-pointer hover:bg-[#1a4f1a] focus:outline-none ">
+                            cursor-pointer hover:bg-[#1a4f1a] focus:outline-none">
                         <TbPawFilled className=" mr-2" />
                         <p>Assinar</p>
                         <TbPawFilled className="ml-2" />
