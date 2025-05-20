f47cef7b8d7b5500d9b915d43b9800d17927f21f
erlizaellen <erlizaellen@gmail.com>
2025-05-20 09:11:26 -0300

    cor

--- src/components/PageWrapper/index.jsx
+++ src/components/PageWrapper/index.jsx
@@ -7,7 +7,7 @@ export default function PageWrapper({children}){
     return (
         <div className="w-full h-full min-h-screen flex flex-col">
             <Header/>
-            <div className="W-full h-full min-h-screen bg-gradient-to-t from-[#d8e4dbc9] to-[#5bf356c2] pb-10">
+            <div className="W-full h-full min-h-screen bg-gradient-to-t from-[#a7e2b6c9] to-[#5bf356c2] pb-10">
                 {children}
                 
             </div>
