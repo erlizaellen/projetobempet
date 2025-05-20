05ea256f086d8848e5265bc6a9dedb82293195be
joa242312 <joaogabrielss2006@gmail.com>
2025-05-20 11:28:19 -0300

    responsive

--- src/components/CardVantagens/index.jsx
+++ src/components/CardVantagens/index.jsx
@@ -4,11 +4,11 @@ export default function CardVantagens({ texto, titulo, className }) {
     const [hover, setHover] = useState(false)
 
     return (
-        <div className="w-[50%]  h-full flex">
+        <div className="w-[50%] max-md:w-full  h-full flex">
             <div
                 onMouseOver={ () => setHover(true)}
                 onMouseOut={() => setHover(false)}
-                className={`relative hover:shadow-md overflow-hidden w-[80%] max-md:w-[100] h-[100px] flex flex-col ml-20 bg-[#ffc0cb] rounded-lg mt-10 ${className}`}>
+                className={`relative hover:shadow-md overflow-hidden w-[80%]   flex flex-col ml-20 max-md:ml-2 bg-[#ffc0cb] rounded-lg mt-10 ${className}`}>
                 <div className={`${hover ? "w-[90px] h-[90px]" : "w-[80px] h-[80px]"} rounded-full transition-all ease-in-out duration-300 bg-[#0000003b] absolute top-[-30px] right-[-40px]`}></div>
                 <h1  className="font-bold text-[20px] ml-4 flex justify-center max-w-[60%]  mt-4 bg-amber-100 px-4 rounded-full">{titulo}</h1>
                 <p className="font-sm text-[20px] ml-6 mt-3">
