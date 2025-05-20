4a7f2bb600fbbaf25837bb4d3870144dfdee32dc
ramirospinheiro <ramiropinheiro@gmail.com>
2025-05-20 10:19:10 -0300

    Att footer

--- src/components/Paw/index.jsx
+++ src/components/Paw/index.jsx
@@ -4,7 +4,7 @@ export default function Paw({}){
     return (
 
                 <div className=" flex flex-col items-center justify-center relative  rounded-full cursor-pointer">
-                    <TbPawFilled className="text-[#215f1f] w-[200px] h-[200px]"/>
+                    <TbPawFilled className="text-[#215f1f] w-[180px] h-[180px]"/>
                     <p className="font-bold absolute text-white bottom-10">Quem somos</p>
                 </div>
     )
