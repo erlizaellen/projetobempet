6efb522643f3f8312d1cacdcdad83d4b38190f04
erlizaellen <erlizaellen@gmail.com>
2025-05-22 11:48:00 -0300

    teste NAO oficial do click fora do quem somos

--- src/components/Paw/index.jsx
+++ src/components/Paw/index.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import { TbPawFilled } from "react-icons/tb";
+import CardModalQuem from "../CardModalQuem";
 
-export default function Paw({}){
-    return (
+export default function Paw({}) {
+  const [cardModalQuem, setCardModalQuem] = useState(false);
 
-                <div className=" flex flex-col items-center justify-center relative  rounded-full cursor-pointer">
-                    <TbPawFilled className="text-[#215f1f] w-[180px] h-[180px]"/>
-                    <p className="font-bold absolute text-white bottom-10">Quem somos</p>
-                </div>
-    )
+  return (
+    <div
+    onClick={() => setCardModalQuem(true)}
+    className=" flex flex-col items-center justify-center relative  rounded-full cursor-pointer">
+      <TbPawFilled className="text-[#215f1f] w-[180px] h-[180px]" />
+      <p className="font-bold absolute text-white bottom-10">Quem somos</p>
+      {cardModalQuem && (
+        <CardModalQuem onClose={() => setCardModalQuem(false)} />
+      )}
+    </div>
+  );
 }
