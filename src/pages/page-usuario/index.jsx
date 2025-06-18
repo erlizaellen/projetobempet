f6c094f44aba5b4f8e14892d2dc61125951a5fef
ramirospinheiro <ramiropinheiro97@gmail.com>
2025-06-18 12:11:05 -0300

    put no perfil de usuario para atualizar telefone

--- src/pages/page-usuario/index.jsx
+++ src/pages/page-usuario/index.jsx
@@ -3,28 +3,30 @@ import PageWrapper from "@/components/PageWrapper";
 import CardAddPet from "@/components/CardAddPet";
 import { useRouter } from "next/router";
 import instance from "@/api/instance";
+import { toast } from "react-toastify";
 
 
 
 export default function PageUsuario() {
   const [mostrarModal, setMostrarModal] = useState(false);
-  const [user,setUser] = useState({})
+  const [user, setUser] = useState({})
   const [pets, setPets] = useState([])
+  const [phone, setPhone] = useState('')
   const router = useRouter();
   const { id } = router.query;
 
-  
+
 
   useEffect(() => {
     const token = localStorage.getItem("token");
 
     if (!token) {
       console.warn("Token não encontrado. Redirecionando para login.");
-      router.push("/login"); 
+      router.push("/login");
       return;
     }
 
-    async function getUserData(){
+    async function getUserData() {
       const response = await instance.get("/userprofile")
       setUser(response.data)
     }
@@ -37,7 +39,19 @@ export default function PageUsuario() {
     getUserData()
     getPetsUser()
   }, [id, router]);
-      
+
+  async function updateUser() {
+    try {
+      const res = await instance.put(`/users/${user.id}`, {
+        telefone: phone
+      })
+
+      return toast.success("vapo")
+    } catch (error) {
+      console.log(error)
+      return toast.error("pedo")
+    }
+  }
 
   return (
     <PageWrapper>
@@ -57,7 +71,9 @@ export default function PageUsuario() {
             <p className="text-green-900 font-bold text-xl">Dados do Usuário</p>
             <p className="text-green-800">Informações do tutor:</p>
           </div>
-          <button className="bg-white border border-green-700 text-green-700 px-4 py-1 rounded hover:bg-green-50">
+          <button
+            onClick={updateUser}
+            className="bg-white border border-green-700 text-green-700 px-4 py-1 rounded hover:bg-green-50">
             Editar
           </button>
         </div>
@@ -83,7 +99,10 @@ export default function PageUsuario() {
             <label className="mb-2 text-green-700">Telefone</label>
             <input
               type="telefone"
-              placeholder="(11) 99999-9999"
+              placeholder={user.telefone}
+              onChange={
+                (ev) => setPhone(ev.target.value)
+              }
               className="p-2 rounded border border-green-300 outline-none"
             />
           </div>
@@ -106,52 +125,58 @@ export default function PageUsuario() {
         </div>
 
         {/* Pet exemplo */}
-        <div className="bg-white rounded-lg p-4 flex flex-col gap-4 w-full ">
-          <div className="flex items-center justify-between">
-            <div className="flex flex-col gap-4">
-              <div className="rounded-full bg-orange-200 mr-4 flex items-center justify-center text-xl"></div>
+        {/* {pets.map((pet) => { */}
+          {/* return ( */}
+            <div className="bg-white rounded-lg p-4 flex flex-col gap-4 w-full ">
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-4">
+                  <div className="rounded-full bg-orange-200 mr-4 flex items-center justify-center text-xl"></div>
+                  <div className="flex gap-4">
+                    <input
+                      type="text"
+                      placeholder="Nome do pet"
+                      className="p-2 border border-orange-300 rounded text-orange-800 outline-none w-full"
+                    />
+                    <input
+                      type="text"
+                      placeholder="Raça"
+                      className="p-2 border border-orange-300 rounded text-orange-800 outline-none w-full"
+                    />
+                  </div>
+                </div>
+                <div className="flex gap-2">
+                  <button className="bg-white border border-orange-400 text-orange-400 p-1 rounded hover:bg-orange-50">
+                    Editar
+                  </button>
+                  <button className="bg-white border border-red-400 text-red-400 p-1 rounded hover:bg-red-50">
+                    Excluir
+                  </button>
+                </div>
+              </div>
+
               <div className="flex gap-4">
-                <input
-                  type="text"
-                  placeholder="Nome do pet"
-                  className="p-2 border border-orange-300 rounded text-orange-800 outline-none w-full"
-                />
-                <input
-                  type="text"
-                  placeholder="Raça"
-                  className="p-2 border border-orange-300 rounded text-orange-800 outline-none w-full"
-                />
+                <div className="bg-orange-50 rounded p-3 flex-1 flex flex-col w-full">
+                  <label className="text-sm text-orange-600 mb-1">Idade</label>
+                  <input
+                    type="text"
+                    placeholder="Ex: 3 anos"
+                    // value={pet.nascimento}
+                    className="p-2 border border-orange-300 rounded text-orange-800 outline-none w-full"
+                  />
+                </div>
+                <div className="bg-orange-50 rounded p-3 flex-1 flex flex-col w-full">
+                  <label className="text-sm text-orange-600 mb-1">Peso</label>
+                  <input
+                    type="text"
+                    // value={pet.peso}
+                    placeholder="Ex: 30kg"
+                    className="p-2 border border-orange-300 rounded text-orange-800 outline-none w-full"
+                  />
+                </div>
               </div>
             </div>
-            <div className="flex gap-2">
-              <button className="bg-white border border-orange-400 text-orange-400 p-1 rounded hover:bg-orange-50">
-                Editar
-              </button>
-              <button className="bg-white border border-red-400 text-red-400 p-1 rounded hover:bg-red-50">
-                Excluir
-              </button>
-            </div>
-          </div>
-
-          <div className="flex gap-4">
-            <div className="bg-orange-50 rounded p-3 flex-1 flex flex-col w-full">
-              <label className="text-sm text-orange-600 mb-1">Idade</label>
-              <input
-                type="text"
-                placeholder="Ex: 3 anos"
-                className="p-2 border border-orange-300 rounded text-orange-800 outline-none w-full"
-              />
-            </div>
-            <div className="bg-orange-50 rounded p-3 flex-1 flex flex-col w-full">
-              <label className="text-sm text-orange-600 mb-1">Peso</label>
-              <input
-                type="text"
-                placeholder="Ex: 30kg"
-                className="p-2 border border-orange-300 rounded text-orange-800 outline-none w-full"
-              />
-            </div>
-          </div>
-        </div>
+          
+        {/* )})} */}
       </div>
 
       {/* Modal para adicionar pet */}
